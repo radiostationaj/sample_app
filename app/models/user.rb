cefa42aed745cfc833e6ef3ae07d4663847de089
AJ Barry <ajslivinill@hotmail.com>
2011-04-10 22:12:46 -0400

    On to Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,8 +47,10 @@ class User < ActiveRecord::Base
   private
  
     def encrypt_password
-      self.salt = make_salt if new_record?
-      self.encrypted_password = encrypt(password)
+      if password.present?
+        self.salt = make_salt if new_record?
+        self.encrypted_password = encrypt(password)
+      end
     end
 
     def encrypt(string)
